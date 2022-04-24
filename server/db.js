9546a9a594bd07b9d59005e90d6660428cafa58e
Aaron K <aaron.kim.dev@gmail.com>
2022-04-24 16:29:31 -0700

    removing env

--- server/db.js
+++ server/db.js
@@ -2,11 +2,6 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // user: "highwind",
-  // password: "",
-  // host: "localhost",
-  // port: 5432,
-  // database: "perntodo"
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   host: process.env.DB_HOST,
