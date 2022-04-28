d7c52ebdb925bcaee2d06e873249419c404bb1b8
Aaron K <aaron.kim.dev@gmail.com>
2022-04-27 22:17:44 -0700

    minor

--- db.js
+++ db.js
@@ -5,27 +5,27 @@ require("dotenv").config(); // Load .env data into process.env
 
 
 // Method 1
-// const devConfig = {
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   port: process.env_PG_PORT,
-// };
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env_PG_PORT,
+};
 
-// const proConfig = {
-//   connectionString: process.env.DATABASE_URL // heroku addons
-// };
-// const pool = new Pool(
-//   process.env.NODE_ENV === "production" ? proConfig : devConfig
-// );
+const proConfig = {
+  connectionString: process.env.DATABASE_URL // heroku addons
+};
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 // Method 2
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const proConfig = process.env.DATABASE_URL; // heroku addons
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-});
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const proConfig = process.env.DATABASE_URL; // heroku addons
+// const pool = new Pool({
+//   connectionString:
+//     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+// });
 
 module.exports = pool;
